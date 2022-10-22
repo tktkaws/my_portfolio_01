d461e98710646fcc66fa3ecf2da16c80be2b8b35
takayuki-takahashi-dic <ttkaws15@gmail.com>
2022-10-22 13:09:09 +0900

    【環境構築】first commit

---
+++ templates/_l-footer.php
@@ -0,0 +1,5 @@
+<footer class="l-footer">
+    <p class="l-footer__text">©2022 ZAWA-ZAWA
+        <span class="l-footer__subtext">Inc.</span>
+    </p>
+</footer>
---
+++ templates/_l-header.php
@@ -0,0 +1,66 @@
+<header class="l-header">
+    <div class="l-header__logo">
+        <a href="<?php echo esc_url(home_url('/')); ?>">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/logo.svg" alt="ZAWAZAWA inc." width="447"
+                height="59" />
+        </a>
+    </div>
+
+    <nav class="l-header__gnav">
+        <ul class="l-header__lists">
+            <li class="l-header__link">
+                <a href="<?php echo esc_url(home_url('/')); ?>" class="l-header__nav-menu-link">home</a>
+            </li>
+            <li class="l-header__link">
+                <a href="<?php echo esc_url(home_url('/')); ?>#about" class="l-header__nav-menu-link">
+                    company</a>
+            </li>
+            <li class="l-header__link">
+                <a href="<?php echo esc_url(home_url('/')); ?>#product" class="l-header__nav-menu-link">
+                    product</a>
+            </li>
+            <li class="l-header__link">
+                <a href="<?php echo esc_url(home_url('/')); ?>#news" class="l-header__nav-menu-link">
+                    news</a>
+            </li>
+            <li class="l-header__link">
+                <a href="<?php echo esc_url(home_url('/')); ?>#info" class="l-header__nav-menu-link">
+                    contact</a>
+            </li>
+        </ul>
+    </nav>
+
+    <button class="l-header__navbtn" onClick="document.querySelector('body').classList.toggle('open')">
+        <span class="l-header__navbtn-bar"></span>
+        <span class="l-header__sr-only">MENU</span>
+    </button>
+
+    <nav class="l-header__nav">
+        <div class="l-header__nav-logo">
+            <a href="<?php echo esc_url(home_url('/')); ?>">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/logo.svg" alt="" />
+            </a>
+        </div>
+        <ul class="l-header__nav-menu">
+            <li class="l-header__nav-menu-list">
+                <a href="<?php echo esc_url(home_url('/')); ?>" class="l-header__nav-menu-link">home</a>
+            </li>
+            <li class="l-header__nav-menu-list">
+                <a href="<?php echo esc_url(home_url('/')); ?>#about" class="l-header__nav-menu-link">
+                    company</a>
+            </li>
+            <li class="l-header__nav-menu-list">
+                <a href="<?php echo esc_url(home_url('/')); ?>#product" class="l-header__nav-menu-link">
+                    product</a>
+            </li>
+            <li class="l-header__nav-menu-list">
+                <a href="<?php echo esc_url(home_url('/')); ?>#news" class="l-header__nav-menu-link">
+                    news</a>
+            </li>
+            <li class="l-header__nav-menu-list">
+                <a href="<?php echo esc_url(home_url('/')); ?>#info" class="l-header__nav-menu-link">
+                    contact</a>
+            </li>
+        </ul>
+    </nav>
+</header>
